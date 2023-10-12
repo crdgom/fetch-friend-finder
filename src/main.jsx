349f22f545ca5b1e3426251507621f3ff0e46775
carlos <crdgom@gmail.com>
2023-10-12 14:18:08 -0600

    Test complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -10,6 +11,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
+    <StrictMode>
     <App />
-
+    </StrictMode>
 )
